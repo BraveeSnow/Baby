377fdadfa082f9a66534d14e411f7be28f47cac2
David Long <david.long@snows.world>
2024-04-02 00:20:32 -0400

    Baby tests

---
+++ src/test/java/world/snows/baby/Basic.java
@@ -0,0 +1,29 @@
+package world.snows.baby;
+
+import org.antlr.v4.runtime.BufferedTokenStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.Lexer;
+import org.antlr.v4.runtime.TokenStream;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Objects;
+
+public class Basic {
+    private static final InputStream PROGRAM = Thread.currentThread().getContextClassLoader().getResourceAsStream("basic.baby");
+
+    @BeforeAll
+    static void isReadable() {
+        assert PROGRAM != null;
+    }
+
+    @Test
+    void runBasic() throws IOException {
+        Lexer lex = new BabyLexer(CharStreams.fromStream(Objects.requireNonNull(PROGRAM)));
+        TokenStream tokens = new BufferedTokenStream(lex);
+        BabyParser parser = new BabyParser(tokens);
+        parser.program().exp.evaluate(new Interpreter());
+    }
+}
