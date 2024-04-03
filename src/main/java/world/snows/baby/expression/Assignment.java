fa1ab0f13730b1b01f92d39c2319a42c99600908
David Long <david.long@snows.world>
2024-04-03 17:06:35 -0400

    Check for proper grammar in assignments

--- src/main/java/world/snows/baby/expression/Assignment.java
+++ src/main/java/world/snows/baby/expression/Assignment.java
@@ -1,15 +1,26 @@
 package world.snows.baby.expression;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import world.snows.baby.Interpreter;
 import world.snows.baby.type.Value;
 
+import java.util.Objects;
+
 public class Assignment implements Expression {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Assignment.class);
+    private static final String VOWELS = "AEIOUaeiou";
+
     private final String identifier;
     private final Expression assignable;
 
-    public Assignment(String name, Expression expr) {
+    public Assignment(String name, Expression expr, String connector) {
         identifier = name;
         assignable = expr;
+
+        if ((VOWELS.indexOf(name.charAt(0)) == -1) == Objects.equals(connector, "an")) {
+            LOGGER.warn("Incorrect grammar in assignment of {}, please use '{}' instead.", identifier, connector.equals("an") ? "a" : "an");
+        }
     }
 
     @Override
